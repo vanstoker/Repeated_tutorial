20e943da4c5a7aaf2e7ec1f69975cbcb20b0c7ce
vanstoker <vanstoker@mail.ru>
2021-09-13 17:12:57 +0300

    Second

---
+++ Gemfile
@@ -0,0 +1,6 @@
+sourse "https://rubygems.org"
+
+gem "roda", "~> 2.1.0"
+gem "tilt", "~>2.0.1"
+gem "erubis", "~> 2.7.0"
+
---
+++ myapp.rb
@@ -0,0 +1,22 @@
+require "roda"
+
+class Myapp < Roda
+  plugin :static, ["/images", "/css", "/js"]
+  plugin :render
+  plugin :head
+
+
+  route do |r|
+    r.root do
+      view("homepage")
+    end
+
+    r.get "about" do
+      view("about")
+    end
+
+    r.get "contact" do
+      view("contact")
+    end
+  end
+end
